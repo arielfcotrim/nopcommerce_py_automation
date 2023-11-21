6bec659414e26509ad77fc71de5fc894876de7d4
arielfcotrim <arielforner@gmail.com>
2023-11-21 22:38:46 +0200

    added comments to class

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -1,25 +1,38 @@
 from selenium import webdriver
 
 
+# Define a class for the login page
 class LoginPage:
-    text_box_username_id = "Email"
-    text_box_password_id = "Password"
-    button_login_xpath = "//input[@class='button-1 login-button']"
-    button_logout_linktext = "Logout"
+    # Define class-level variables to store element identifiers
+    text_box_username_id = "Email"  # ID of the username input field
+    text_box_password_id = "Password"  # ID of the password input field
+    button_login_xpath = "//input[@class='button-1 login-button']"  # XPath for the login button
+    button_logout_linktext = "Logout"  # Link text for the logout button
 
+    # Constructor method that initializes the class with a Selenium WebDriver instance
     def __init__(self, driver):
         self.driver = driver
 
+    # Method to set the username in the username input field
     def set_username(self, username):
+        # Clear any existing text in the username input field
         self.driver.find_element_by_id(self.text_box_username_id).clear()
+        # Enter the provided username in the username input field
         self.driver.find_element_by_id(self.text_box_username_id).send_keys(username)
 
+    # Method to set the password in the password input field
     def set_password(self, password):
+        # Clear any existing text in the password input field
         self.driver.find_element_by_id(self.text_box_password_id).clear()
+        # Enter the provided password in the password input field
         self.driver.find_element_by_id(self.text_box_password_id).send_keys(password)
 
+    # Method to click the login button
     def click_login_button(self):
+        # Locate the login button using the defined XPath and click it
         self.driver.find_element_by_xpath(self.button_login_xpath).click()
 
+    # Method to click the logout button
     def click_logout_button(self):
+        # Locate the logout button by its link text and click it
         self.driver.find_element_by_link_text(self.button_logout_linktext).click()
