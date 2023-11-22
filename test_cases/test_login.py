53a7f0713727c88cd5a795c48439dfe7167cc52e
ariel forner win <arielforner@gmail.com>
2023-11-22 09:45:17 +0200

    construct setup object

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from selenium import webdriver
 from page_objects.login_page import LogInPage
+import config_tests
 
 
 class TestLogIn:
@@ -10,10 +11,12 @@ class TestLogIn:
     # username = "admin@yourstore.com"
     password = os.environ.get('PASSWORD')
     # password = "admin"
+    setup = config_tests.setup()
 
     def test_home_page_title(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
+
         expected_title = "Your store. Login"
         actual_title = self.driver.title
 
@@ -29,16 +32,20 @@ class TestLogIn:
     def test_log_in(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
+
         self.login_page = LogInPage(self.driver)
         self.login_page.set_username(self.username)
         self.login_page.set_password(self.password)
         self.login_page.click_login_button()
+
         expected_title = "Dashboard / nopCommerce administration"
         actual_title = self.driver.title
-        self.driver.quit()
 
         if actual_title == expected_title:
             assert True
 
         else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_log_in" + ".png")
             assert False
+
+        self.driver.quit()
